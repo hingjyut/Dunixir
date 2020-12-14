fab54600205de0480f5ffa647a98ad8444ddf891
Etienne Ferrieux <etienne.ferrieux@imt-atlantique.net>
2020-12-14 12:39:45 +0100

    added repos

--- blockchain_processor/lib/blockchain_processor/document_processor.ex
+++ blockchain_processor/lib/blockchain_processor/document_processor.ex
@@ -1,8 +1,7 @@
 defmodule DocumentProcessor do
 
-  def process_identity(uid, pub, block_uid, block_stamp) do
-
-    changeset = IIndex.changeset(%IIndex{}, %{op: "CREATE", uid: uid, pub: pub, created_on: block_uid, written_on: block_stamp, member: true, was_member: false, kick: false} )
+  def process_identity(%{uid: uid, pub: pub, block_uid: block_uid, block_stamp: block_stamp}) do
+    changeset = LocalIIndex.changeset(%LocalIIndex{}, %{op: "CREATE", uid: uid, pub: pub, created_on: block_uid, written_on: block_stamp, member: true, was_member: false, kick: false} )
     Repo.insert(changeset)
   end
 
