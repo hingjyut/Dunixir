a7546695efe147e34ab669d4aea1c4713074a9fe
Quentin PUBERT <quentin.pubert@outlook.fr>
2020-12-19 18:24:05 +0100

    simple handshake now works (tested with a local up-to-date duniter node)

--- handshake/lib/ws2p_connection.ex
+++ handshake/lib/ws2p_connection.ex
@@ -1,5 +1,5 @@
 defmodule WS2P.Connection do
-  use GenServer
+  use GenServer, restart: :transient
   require Logger
 
   def start_link({:connect, address_and_port, path}) do
@@ -35,6 +35,7 @@ defmodule WS2P.Connection do
   defp loop(connection) do
     case connection |> WS2P.Connection.receive_object() do
       {_object, :answer, answer} ->
+        Logger.info("Sent back answer to remote node")
         connection |> WS2P.Connection.send_object(answer)
 
       _ ->
@@ -46,16 +47,69 @@ defmodule WS2P.Connection do
 
   def handle_object(
         %{
+          "auth" => "OK",
+          "sig" => _sig
+        } = ack_object,
+        %{
+          socket: socket
+        } = state
+      ) do
+    Logger.info("Received OK from node #{inspect(Socket.Web.remote!(socket))}")
+    {:reply, {ack_object, :noanswer}, state}
+  end
+
+  def handle_object(
+        %{
+          "auth" => "ACK",
+          "pub" => _pub,
+          "sig" => _sig
+        } = ack_object,
+        %{
+          socket: socket,
+          keypair: {my_sec, my_pub},
+          currency_name: currency_name,
+          local_challenge: local_challenge
+        } = state
+      ) do
+    Logger.info("Received ACK from node #{inspect(Socket.Web.remote!(socket))}")
+
+    my_pub_as_base58 = my_pub |> Base58.encode()
+    raw_format = "WS2P:OK:#{currency_name}:#{my_pub_as_base58}:#{local_challenge}"
+    signature = Ed25519.signature(raw_format, my_sec, my_pub)
+
+    ok_object = %{
+      "auth" => "OK",
+      "sig" => signature |> Base.encode64()
+    }
+
+    Logger.info("Sent OK to node #{inspect(Socket.Web.remote!(socket))}\n#{raw_format}")
+    {:reply, {ack_object, :answer, ok_object}, state}
+  end
+
+  def handle_object(
+        %{
           "auth" => "CONNECT",
           "challenge" => challenge,
-          "currency" => currency,
-          "pub" => pub,
-          "sig" => sig
+          "currency" => _currency,
+          "pub" => _pub,
+          "sig" => _sig
         } = connect_object,
-        %{socket: socket} = state
+        %{socket: socket, keypair: {my_sec, my_pub}, currency_name: currency_name} = state
       ) do
     Logger.info("Received CONNECT from node #{inspect(Socket.Web.remote!(socket))}")
-    {:reply, {connect_object, :noanswer}, state}
+
+    my_pub_as_base58 = my_pub |> Base58.encode()
+    raw_format = "WS2P:ACK:#{currency_name}:#{my_pub_as_base58}:#{challenge}"
+    signature = Ed25519.signature(raw_format, my_sec, my_pub)
+
+    ack_object = %{
+      "auth" => "ACK",
+      "pub" => my_pub_as_base58,
+      "sig" => signature |> Base.encode64()
+    }
+
+    Logger.info("Sent ACK to node #{inspect(Socket.Web.remote!(socket))}\n#{raw_format}")
+    {:reply, {connect_object, :answer, ack_object}, state}
   end
 
   def handle_object(object, state) do
@@ -65,13 +119,16 @@ defmodule WS2P.Connection do
   end
 
   def handle_packet(:close, %{socket: socket} = state) do
-    Logger.info("Remote #{inspect Socket.Web.remote!(socket)} closed socket connection")
+    Logger.info("Remote #{inspect(Socket.Web.remote!(socket))} closed socket connection")
     Socket.Web.close(socket)
     {:stop, :shutdown, state}
   end
 
   def handle_packet({:close, code, reason}, %{socket: socket} = state) do
-    Logger.debug("Remote #{inspect Socket.Web.remote!(socket)} closed socket connection with code \"#{code}\"")
+    Logger.debug(
+      "Remote #{inspect(Socket.Web.remote!(socket))} closed socket connection with code \"#{code}\""
+    )
+
     Logger.debug("Reason: \"#{reason}\"")
     Socket.Web.close(socket)
     {:stop, :shutdown, state}
@@ -114,7 +171,7 @@ defmodule WS2P.Connection do
   end
 
   def handle_cast(
-        {:send_connect, {sec, pub} = _keypair, currency_name},
+        {:send_connect, {sec, pub} = keypair, currency_name},
         %{socket: socket} = state
       ) do
     challenge = UUID.uuid4() <> UUID.uuid4()
@@ -131,8 +188,16 @@ defmodule WS2P.Connection do
 
     {:noreply, state} = handle_cast({:send_object, connect_object}, state)
 
-    Logger.info("Sent CONNECT to node #{inspect(Socket.Web.remote!(socket))}")
-    {:noreply, Map.put_new(state, :local_challenge, challenge)}
+    Logger.info("Sent CONNECT to node #{inspect(Socket.Web.remote!(socket))}\n#{raw_format}")
+
+    new_state =
+      Map.merge(state, %{
+        local_challenge: challenge,
+        keypair: keypair,
+        currency_name: currency_name
+      })
+
+    {:noreply, new_state}
   end
 
   def receive_object(connection) do
