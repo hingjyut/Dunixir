5b22884f7cfd8fae9c9c00da7d49984f3298e573
Etienne Ferrieux <etienne.ferrieux@gmail.com>
2021-01-04 13:58:45 +0100

    dunixir 2 without Ecto

---
+++ dunixir_2/lib/constants.ex
@@ -0,0 +1,8 @@
+defmodule Constants do
+  def medianTime, do: 10
+  def msValidity, do: 10
+  def msPeriod, do: 10
+  def sigValidity, do: 10
+  def sigPeriod, do: 10
+  def sigReplay, do: 10
+end
---
+++ dunixir_2/test/dunixir2_test.exs
@@ -0,0 +1,8 @@
+defmodule Dunixir2Test do
+  use ExUnit.Case
+  doctest Dunixir2
+
+  test "greets the world" do
+    assert Dunixir2.hello() == :world
+  end
+end
