22e2b9135935b301c98646ae62dc70a3f6062b66
Quentin PUBERT <quentin.pubert@imt-atlantique.net>
2021-01-06 10:56:29 +0100

    removed simple_server (old code)

--- simple_server/priv/repo/migrations/20201102164836_create_sandbox_id_view.exs
+++
@@ -1,9 +0,0 @@
-defmodule Duniter.Repo.Migrations.CreateSandboxIdView do
-  use Ecto.Migration
-
-  def change do
-    execute("""
-    CREATE VIEW sandbox_idty AS SELECT * FROM identities WHERE identities.member = false;
-    """)
-  end
-end
--- simple_server/priv/repo/migrations/20201102184234_create_sandbox_certs.exs
+++
@@ -1,9 +0,0 @@
-defmodule Duniter.Repo.Migrations.CreateSandboxCerts do
-  use Ecto.Migration
-
-  def change do
-    execute("""
-    CREATE VIEW sandbox_certs AS SELECT * FROM certifications WHERE certifications.written = false;
-    """)
-  end
-end
