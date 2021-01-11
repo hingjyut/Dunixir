fcafa7a3a71d46eead36e02d6efe478e606917b7
yesunch <sun.ye@hotmail.com>
2021-01-11 09:20:33 +0100

    For issue 32 & 29, v1.0
    edited doctest

---
+++ lib/block/validation/local/check_previous_issuer.ex
@@ -0,0 +1,5 @@
+defmodule Block.Validation.Local.PreIssuer do
+  def valid(block) do
+    not (block["number"] == 0 and block["previousIssuer"] != nil) and not (block["number"] > 0 and block["previousIssuer"] == nil)
+  end
+end
