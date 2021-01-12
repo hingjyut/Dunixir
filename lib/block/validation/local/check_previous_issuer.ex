6177f9cf2b3c50b4f4562e7247a2257579bc2c49
SUN Ye <ye.sun@imt-atlantique.net>
2021-01-12 08:18:36 +0000

    Update check_previous_issuer.ex, added indent

--- lib/block/validation/local/check_previous_issuer.ex
+++ lib/block/validation/local/check_previous_issuer.ex
@@ -1,5 +1,7 @@
 defmodule Block.Validation.Local.PreIssuer do
   def valid(block) do
-    not (block["number"] == 0 and block["previousIssuer"] != nil) and not (block["number"] > 0 and block["previousIssuer"] == nil)
+    not (block["number"] == 0 and block["previousIssuer"] != nil) 
+    and 
+    not (block["number"] > 0 and block["previousIssuer"] == nil)
   end
 end
