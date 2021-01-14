2c4f360f2814648d1707524e4776b3b35b1fd680
Thomas Barbier <thomas.barbier@imt-atlantique.net>
2021-01-14 12:34:00 +0100

    lancement de création automatique et serveur http blockchain/block fait

--- lib/bma/blockchain/block.ex
+++ lib/bma/blockchain/block.ex
@@ -1,5 +1,14 @@
 defmodule BMA.Blockchain.Block do
+
+    require Logger
+
+
     def get(number) do
-        Poison.encode!("#TODO"<>number)
+        {n, _} = :string.to_integer(number)
+        {:ok, :block} = :dets.open_file(:block , [{:file, 'data/block'} , {:type, :set}])
+        case :dets.lookup(:block, n) do 
+            [] -> Poison.encode!(%{"ucode" => 2011, "message" => "Block not found"})
+            [{n, block}] -> JSON.encode!(block)
+        end
     end
 end
