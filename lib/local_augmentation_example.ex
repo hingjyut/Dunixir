35c3b489906e8d379f2af06533900568f7a07afe
FAMECHON Olivier <famechonolivier@macbook-pro-de-famechon.home>
2021-01-19 14:53:59 +0100

    add membersCount and test

--- lib/local_augmentation_example.ex
+++ lib/local_augmentation_example.ex
@@ -8,6 +8,7 @@ defmodule Index.Augmentation do
         :ets.insert(local_iindex,{key,Map.merge(entry,%{pubUnique: false})})
       end
     end
+
     def excludedIsMember(local_iindex,global_iindex,key) do
       [{key,entry}] = :ets.lookup(local_iindex,key) ## key is supposed to be unique for local tables
       if entry.member != false do
@@ -19,6 +20,22 @@ defmodule Index.Augmentation do
           !member
         end))
         :ets.insert(local_iindex,{key,Map.merge(entry,%{excludedIsMember: excludedIsMember})})
+      end
+    end
+  end
+
+  defmodule BIndex do
+    def membersCount(local_iindex, global_bindex, local_bindex) do
+      key = :ets.first(local_bindex)
+      [{key, head}] = :ets.lookup(local_bindex,key)
+
+      if head.number == 0 do
+        count = length(:ets.match(local_iindex,{:"$1",%{member: true}}))
+        :ets.insert(local_bindex,{key,Map.merge(head, %{membersCount: count})})
+      else
+        [{_key2, last_block}] = :dets.match_object(global_bindex,{:_, %{number: head.number - 1}})
+        count = last_block.membersCount + length(:ets.match(local_iindex,{:"$1",%{member: true}})) - length(:ets.match(local_iindex,{:"$1",%{member: false}}))
+        :ets.insert(local_bindex,{key,Map.merge(head, %{membersCount: count})})
       end
     end
   end
