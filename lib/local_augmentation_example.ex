928cab0f9f1c2ebbad57fc6c5bd124a37da7a2c6
yesunch <sun.ye@hotmail.com>
2021-01-19 08:58:04 +0100

    added solution for uidUnique

--- lib/local_augmentation_example.ex
+++ lib/local_augmentation_example.ex
@@ -8,6 +8,17 @@ defmodule Index.Augmentation do
         :ets.insert(local_iindex,{key,Map.merge(entry,%{pubUnique: false})})
       end
     end
+    
+    def uidUnique(local_iindex,global_iindex,key) do
+      ## uid is supposed to be unique for local tables
+      [{key,entry}] = :ets.lookup(local_iindex,key) 
+      ##match global index entries on uid
+      if :dets.match(global_iindex,{:"$1",%{uid: entry.uid}}) == [] do 
+       :ets.insert(local_iindex,{key,Map.merge(entry,%{uidUnique: true})}) 
+      else
+        :ets.insert(local_iindex,{key,Map.merge(entry,%{uidUnique: false})})
+      end
+    end
   end
 
 end
