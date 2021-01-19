6bc0519af8c48dca9120d0963583a0835e34e67f
FAMECHON Olivier <famechonolivier@macbook-pro-de-famechon.home>
2021-01-19 12:16:28 +0100

    excludedIsMember validation rule

--- lib/local_augmentation_example.ex
+++ lib/local_augmentation_example.ex
@@ -8,6 +8,18 @@ defmodule Index.Augmentation do
         :ets.insert(local_iindex,{key,Map.merge(entry,%{pubUnique: false})})
       end
     end
+    def excludedIsMember(local_iindex,global_iindex,key) do
+      [{key,entry}] = :ets.lookup(local_iindex,key) ## key is supposed to be unique for local tables
+      if entry.member != false do
+        :ets.insert(local_iindex,{key,Map.merge(entry,%{excludedIsMember: true})})
+      else
+        list_pub = :dets.match(global_iindex,{:"$1",%{pub: entry.pub, member: :"$2"}})
+        excludedIsMember = is_nil(Enum.find_value(list_pub, fn elt ->
+          [_pub,member] = elt
+          !member
+        end))
+        :ets.insert(local_iindex,{key,Map.merge(entry,%{excludedIsMember: excludedIsMember})})
+      end
+    end
   end
-
 end
