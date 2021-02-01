3f092269678a47a3bf771fb740c689130bff5c10
Julien29121998 <benardjr@gmail.com>
2021-02-01 16:27:32 +0100

    merge manually #76 from yesun

--- lib/bma/blockchain/with/newcomers.ex
+++ lib/bma/blockchain/with/newcomers.ex
@@ -1,5 +1,25 @@
 defmodule BMA.Blockchain.With.Newcomers do
+
+    @doc"""
+    Get a list of block ID of blocks with non-empty identitis
+    """
     def get do
-        Poison.encode!("#TODO")
-    end
+        {:ok, blocks} = :dets.open_file(:block , [{:file, 'data/block'} , {:type, :set}])
+        list = :dets.traverse(
+            blocks,
+            fn{_, block} ->
+                if (block["identities"] != []) do
+                    {:continue, block["number"]}
+                else
+                    :continue
+                end
+            end
+        )
+
+        Poison.encode!(
+            %{result:
+            %{blocks: list
+            }})
+        end
+
 end
