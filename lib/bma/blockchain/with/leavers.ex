a1554c2b62b494a60785ccc81749d3fda2074c63
NAMYST Romain <romain.namyst@imt-atlantique.net>
2021-02-04 13:49:26 +0000

    Implements: #80 #81

--- lib/bma/blockchain/with/leavers.ex
+++ lib/bma/blockchain/with/leavers.ex
@@ -1,5 +1,18 @@
 defmodule BMA.Blockchain.With.Leavers do
     def get do
-        Poison.encode!("#TODO")
+        {:ok, blocks} = :dets.open_file(:block , [{:file, 'data/block'} , {:type, :set}])
+        result = []
+        result = result ++ :dets.traverse(
+            :block,
+            fn {_, value} ->
+              if(value["leavers"]) != []) do
+                  {:continue, value["number"]}
+              else
+                  :continue
+              end
+            end
+                 )
+        JSON.encode!([result: [blocks: result]])
     end
+
 end
--- lib/bma/blockchain/with/revoked.ex
+++ lib/bma/blockchain/with/revoked.ex
@@ -1,5 +1,18 @@
 defmodule BMA.Blockchain.With.Revoked do
     def get do
-        Poison.encode!("#TODO")
+        {:ok, :block} = :dets.open_file(:block, [{:file, 'data/block'}, {:type, :set}])
+        result = []
+        result = result ++ :dets.traverse(
+            :block,
+            fn {_, value} ->
+              if(value["revoked"]) != []) do
+                  {:continue, value["number"]}
+              else
+                  :continue
+              end
+            end
+                 )
+        JSON.encode!([result: [blocks: result]])
     end
+
 end
