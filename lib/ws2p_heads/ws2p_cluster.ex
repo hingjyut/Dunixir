cf1e910293ae57f5460560a6f8d4d4a0fd66a49b
x18zhan2 <hingjyutcheung@hotmail.com>
2021-02-15 12:32:43 +0100

    worked headscache, some todos left in ws2p_cluster

--- lib/ws2p_heads/ws2p_cluster.ex
+++ lib/ws2p_heads/ws2p_cluster.ex
@@ -237,20 +237,3 @@ end
 #           step: String.t()
 #         }
 # end
-
-"""
-%{
-  "body" => %{
-    "heads" => [
-      %{
-        "message" => "WS2POCAIC:HEAD:1:HAy1hLpHfqrG3xsZRoBVkNigGQZnDfJK2az5MeRYtyNb:694027-0000988886FA69E41386B0E992B66DB4FE77BA4A5B29B5BBD04F85DFEDC80CF3:c4df202c:duniter:1.8.1:1",
-        "messageV2" => "WS2POCAIC:HEAD:2:HAy1hLpHfqrG3xsZRoBVkNigGQZnDfJK2az5MeRYtyNb:694027-0000988886FA69E41386B0E992B66DB4FE77BA4A5B29B5BBD04F85DFEDC80CF3:c4df202c:duniter:1.8.1:1:9:9",
-        "sig" => "FyUd1YZPFK483p9xJDveDeSPDQUYlr90FEgXDuxGajfc7t7pw7L2zEV/In0zTNX4O+hT8Otan2HTKw//JbkzCQ==",
-        "sigV2" => "GZgbXaVUKrFAg4iTYqh9EqmUg0yr/aZeeLEmuT2i9IFK9OBc+3LwdqbYdIdg9dcl3XwdbAFzkkV6qNqip2iCDg==",
-        "step" => 2
-      }
-    ],
-    "name" => "HEAD"
-  }
-}
-"""
