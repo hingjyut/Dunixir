1624e9b4dc888eb42bf803839f05dccdadc84726
x18zhan2 <hingjyutcheung@hotmail.com>
2021-03-01 17:10:06 +0100

    Updated constants file and created a conf file

--- lib/dunixir/constants.ex
+++ lib/dunixir/constants.ex
@@ -11,3 +11,35 @@ defmodule Constants do
   def sigPeriod, do: 10
   def sigReplay, do: 10
 end
+
+defmodule Constants.Contract do
+  def c, do: 0.007376575
+  def dt, do: 30.4375 * 24 * 3600
+  def dtReeval, do: 30.4375 * 24 * 3600
+  def ud0, do: 100
+  def stepMax, do: 3
+  def msValidity, do: 3600 * 24 * 365
+  def sigValidity, do: 3600 * 24 * 365
+  def sigPeriod, do: 0
+  def sigReplay, do: 0
+  def sigWindow, do: 3600 * 24 * 7
+  def sigQty, do: 5
+  def xpercent, do: 0.9
+  def percentRot, do: 2/3
+  def powDelay, do: 0
+  def avgGenTime, do: 16 * 60
+  def dtDiffEval, do: 10
+  def medianTimeBlocks, do: 20
+  def forSize, do: 100
+  def switchOnHeadAdvance, do: 3
+  def nonWoTPeersLimit, do: 100
+  def txsMempoolSize, do: 200
+  def sigStock, do: 40
+  def idtyWindow, do: 3600 * 24 * 7
+  def msWindow, do: 3600 * 24 * 7
+end
+
+defmodule Constants.PoW do
+  def cpu, do: 0.6
+  def prefix, do: 1
+end
